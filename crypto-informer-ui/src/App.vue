55740406e927209254e93eda4ee46bc970361920
Aldin Mujkic <aldinmujkic44@gmail.com>
2018-04-12 00:07:08 +0200

    edit readme

--- crypto-informer-ui/src/App.vue
+++ crypto-informer-ui/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <navigation></navigation>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import navigation from './components/navigation'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    navigation
   }
 }
 </script>
@@ -23,6 +21,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
